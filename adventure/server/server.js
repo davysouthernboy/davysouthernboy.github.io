356ebad5bfb362956b93f9d8c1d73f28e6953e07
Davie Overman <davie.overman@gmail.com>
2026-04-15 19:57:08 -0400

    new games 4-15-2026 19:57

--- adventure/server/server.js
+++ adventure/server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://davysouthernboy-github-io.railway.internal',
     methods: ['GET', 'POST']
   }
 });
